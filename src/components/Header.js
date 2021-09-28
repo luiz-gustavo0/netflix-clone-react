f43d43bf47b4b033038537ef4a32db7a69fa8fad
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-09-28 13:53:53 -0300

    muda logo do header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import './Header.css';
+import logo from '../img/logo-bgremover.png';
+
 const Header = ({ black }) => {
   return (
     <header className={black ? 'black' : ''}>
-      <div className="header--logo">
-        <a href="/">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
-            alt="Netflix"
-          />
+      <div className='header--logo'>
+        <a href='/'>
+          <img src={logo} alt='Netflix' />
         </a>
       </div>
-      <div className="header--user">
-        <a href="/">
+      <div className='header--user'>
+        <a href='/'>
           <img
-            src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
-            alt="Logo usuario"
+            src='https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png'
+            alt='Logo usuario'
           />
         </a>
       </div>
--- src/services/Api.js
+++ src/services/Api.js
@@ -14,56 +14,56 @@ export default {
         slug: 'originals',
         title: 'Originais da Netflix',
         items: await basicFetch(
-          `/discover/tv?with_network=213&language=pt-BR&${process.env.REACT_APP_API_KEY}`,
+          `/discover/tv?with_network=213&language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`,
         ),
       },
       {
         slug: 'trending',
         title: 'Recomendados para Você',
         items: await basicFetch(
-          `/trending/all/week?language=pt-BR&${process.env.REACT_APP_API_KEY}`,
+          `/trending/all/week?language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`,
         ),
       },
       {
         slug: 'toprated',
         title: 'Em Alta',
         items: await basicFetch(
-          `/movie/top_rated?language=pt-BR&${process.env.REACT_APP_API_KEY}`,
+          `/movie/top_rated?language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`,
         ),
       },
       {
         slug: 'action',
         title: 'Ação',
         items: await basicFetch(
-          `/discover/movie?with_genres=28&language=pt-BR&${process.env.REACT_APP_API_KEY}`,
+          `/discover/movie?with_genres=28&language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`,
         ),
       },
       {
         slug: 'comedy',
         title: 'Comédia',
         items: await basicFetch(
-          `/discover/movie?with_genres=35&language=pt-BR&${process.env.REACT_APP_API_KEY}`,
+          `/discover/movie?with_genres=35&language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`,
         ),
       },
       {
         slug: 'horror',
         title: 'Terror',
         items: await basicFetch(
-          `/discover/movie?with_genres=27&language=pt-BR&${process.env.REACT_APP_API_KEY}`,
+          `/discover/movie?with_genres=27&language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`,
         ),
       },
       {
         slug: 'romance',
         title: 'Romace',
         items: await basicFetch(
-          `/discover/movie?with_genres=10749&language=pt-BR&${process.env.REACT_APP_API_KEY}`,
+          `/discover/movie?with_genres=10749&language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`,
         ),
       },
       {
         slug: 'documentary',
         title: 'Documentário',
         items: await basicFetch(
-          `/discover/movie?with_genres=99&language=pt-BR&${process.env.REACT_APP_API_KEY}`,
+          `/discover/movie?with_genres=99&language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`,
         ),
       },
     ];
@@ -74,12 +74,12 @@ export default {
       switch (type) {
         case 'movie':
           info = await basicFetch(
-            `/movie/${movieId}?language=pt-BR&${process.env.REACT_APP_API_KEY}`,
+            `/movie/${movieId}?language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`,
           );
           break;
         case 'tv':
           info = await basicFetch(
-            `/tv/${movieId}?language=pt-BR&${process.env.REACT_APP_API_KEY}`,
+            `/tv/${movieId}?language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`,
           );
           break;
         default:
